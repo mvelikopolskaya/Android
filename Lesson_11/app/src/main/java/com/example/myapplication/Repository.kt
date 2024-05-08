120f8d98dfafd83bf9692f8c7c3d7ce329466da0
Cindrom1 <cindrof@gmail.com>
2024-05-08 16:16:22 +0300

    added Lesson_11

--- Lesson_11/app/src/main/java/com/example/myapplication/Repository.kt
+++ Lesson_11/app/src/main/java/com/example/myapplication/Repository.kt
@@ -3,38 +3,35 @@ package com.example.myapplication
 
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
-import android.content.SharedPreferences
+import android.util.Log
 
-private var prefs : SharedPreferences? = null
-private val editor = prefs?.edit()
-private var localVariable: String? = null
 const val PREFERENCE_NAME = "prefs_name"
 const val TEXT_EDITOR_CONTENT = "text_editor_content"
 
-class Repository {
+class Repository(val context : Context) {
+    private var prefs = context.getSharedPreferences(PREFERENCE_NAME, MODE_PRIVATE)
+    private val editor = prefs.edit()
+    private var localVariable: String? = null
 
     fun saveText(text: String) {
         localVariable = text
-        editor?.putString(TEXT_EDITOR_CONTENT, text)?.apply()
+        editor.putString(TEXT_EDITOR_CONTENT, text).apply()
     }
 
-    fun getText(context: Context) : String {
-        prefs = context.getSharedPreferences(PREFERENCE_NAME, MODE_PRIVATE)
+    fun getText() : String {
         return when {
             getDataFromLocalVariable() != null -> getDataFromLocalVariable()!!
-            getDataFromSharedPreference(context) != null -> getDataFromSharedPreference(context)!!
+            getDataFromSharedPreference() != null -> getDataFromSharedPreference()!!
             else -> context.getString(R.string.data_info)
         }
     }
 
     fun clearText() {
-        editor?.clear()
+        editor.clear().apply()
         localVariable = null
     }
-
-    private fun getDataFromSharedPreference(context: Context): String? {
-        prefs = context.getSharedPreferences(PREFERENCE_NAME, MODE_PRIVATE)
-        return prefs?.getString(TEXT_EDITOR_CONTENT, null)
+    private fun getDataFromSharedPreference(): String? {
+        return prefs.getString(TEXT_EDITOR_CONTENT, null)
     }
 
     private fun getDataFromLocalVariable(): String? {
